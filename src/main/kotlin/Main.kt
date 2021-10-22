8ad7f7f6985188be856643ef9057c4180831f020
Fernando Batista <wiesecredito@gmail.com>
2021-10-22 12:41:12 -0300

    Primeiro commit usando Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,8 @@ fun main(args: Array<String>) {
     println("<???> Opa, eae")
     println("<Rogério> De boas?")
     println("<???> Suaves")
-    println("<Rogério> Qual é o seu nome?")
+    println("<Rogério> Qual o seu nome?")
     println("<???> Meu nome é Fernando")
     println("<Rogério> Opa, prazer em conhecê-lo Fernando")
     println("<Fernando> O prazer é todo meu")
-
 }
